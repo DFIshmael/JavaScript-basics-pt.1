a8d3415aee4a5b40d180150f1af61e4ae35afbd1
Derelle Ishmael <d.f.ishmael@gmail.com>
2020-08-26 17:07:06 -0400

    update

--- script.js
+++ script.js
@@ -3,26 +3,32 @@ console.log('Hello World!!!!');
 var firstName = 'Derelle';
 console.log(firstName);
 
-var lastName = 'Ishmael';
+var lastName = 'Ishmael'; //string
 console.log(lastName);
 
-var fullAge = false;
+var fullAge = false; //boolean
 console.log(fullAge);
 
-var  job;
-console.log(job);
-// undefined value -- var job;
-
-//defined value -- job = 'Teacher'
-job = 'Teacher';
+var  job; // undefined value
 console.log(job);
 
-var e90e = 4;
+
+job = 'Teacher';//defined value 
+console.log(job);
+
+var e90e = 4; //number
 console.log(e90e);
 
 //type coercion/conversion
 var firstName = 'Bob'; //string
-var age = 28;// number
+var age = 28; // number
 console.log(firstName + ' ' + age); //JS automatically converts both types to form string
+
+var job, isMarrried; //undefined values
+job = 'teacher'; // defined string
+isMarrried = false; // boolean
+console.log(isMarrried,job);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarrried); // correct spacing
+
 
 
