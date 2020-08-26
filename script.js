e43a1b2577377f141d99b75826e185893c21af06
Derelle Ishmael <d.f.ishmael@gmail.com>
2020-08-26 13:41:12 -0400

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Hello World!!!!');
+
+var firstName = 'Derelle';
+console.log(firstName);
+
+var lastName = 'Ishmael';
+console.log(lastName);
+
+var fullAge = false;
+console.log(fullAge);
+
+var  job;
+console.log(job);
+// undefined value
+
+job = 'Teacher';
+console.log(job);
