35a98caef0401edbba9a300522175ea6637b0332
Derelle Ishmael <d.f.ishmael@gmail.com>
2020-08-26 21:05:35 -0400

    .

--- script.js
+++ script.js
@@ -30,11 +30,39 @@ isMarrried = false; // boolean
 console.log(isMarrried,job);
 console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarrried); // correct spacing
 
-//variable value updates as you redeclare them
+//variable value updates as you redeclare them. 'variable mutation'
 job = 'driver';
 age = 'twenty eight';
 firstName = 'Nate';
 
 alert(firstName+' is a '+age+' year old '+job); //POP up window in Browser. Not printed in console
 
+console.log(lastName);
 
+prompt('What is his last name?'); //pop up window in browser prompting user for data input.
+var lastName = prompt('What is his last name?'); //variable 'lastName' changes based on user input in browser and logged.
+ console.log(firstName+' ' +lastName);
+ 
+ prompt('How old is he?'); //Don't need to include this step. just var age = prompt('')
+ 
+ var age = prompt('How old is he?');
+ console.log(firstName+' is '+age);
+
+ 
+ //My own examples. VARS dont need paranthesis
+ var time = '2:00PM';
+ var timeofDay = 'afternoon';
+ 
+ console.log('What time of day is it? '+time);
+
+ var time = prompt('What time of day is it?');
+console.log(time);// user input changed time from 2:00PM to 4:00PM
+
+//Var Shortcuts
+var girlCoder, badCoder, clearWater;
+girlCoder = 'Derelle';
+badCoder = false;
+clearWater = true;
+
+console.log('Is '+girlCoder+' a bad '+'coder? '+badCoder);
+console.log('Can '+girlCoder+' walk through water ?'+clearWater);
